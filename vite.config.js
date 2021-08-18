06f87ab57e833c94d2781684e8f4aa9ad26fcf9c
rzola-ie <ryan.zola@idea-evolver.com>
2021-08-18 16:21:17 -0400

    no minifly?

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,21 @@ const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
   build: {
+    minify: false,
     rollupOptions: {
+      include: {
+
+      },
       input: {
         main: resolve(__dirname, 'index.html'),
         room: resolve(__dirname, 'room.html'),
         face: resolve(__dirname, 'face.html')
       }
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      keepNames: true
     }
   }
 })
